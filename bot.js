93dab63c62a0e3c63b31cc1011a712b7a4747b11
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-07-03 02:58:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
- 
+
 client.on('message', message => {
     if (message.content === 'ping') {
-       message.reply('pong');
-       }
+    	message.reply('pong');
+  	}
 });
- 
+
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NTk1ODUzMjA4NDA5MzQxOTUz.XRxHGg.gvBO4lX1P_931aDWzIrhH9MmmXE);//BOT_TOKEN is the Bots token
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
