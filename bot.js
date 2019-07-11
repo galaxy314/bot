54c85cf27d40e94d2ff32ac80edb7a3623173781
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-07-11 14:15:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,37 @@ client.on('message', message => {
     }else if(messageID !== '452904501163196426' && command === 'jah inspire'){
            message.reply('You are not my inspiration'); 
     }
+    if(command === 'jah help'){
+            message.channel.send({embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: "This is an embed",
+            url: "http://google.com",
+            description: "This is a test embed to showcase what they look like and what they can do.",
+            fields: [{
+                name: "Fields",
+                value: "They can have different fields with small headlines."
+              },
+              {
+                name: "Masked links",
+                value: "You can put [masked links](http://google.com) inside of rich embeds."
+              },
+              {
+                name: "Markdown",
+                value: "You can put all the *usual* **__Markdown__** inside of them."
+              }
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "© Example"
+            }
+          }
+        });
+   }
     
 });
 
