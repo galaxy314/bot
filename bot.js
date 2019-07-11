5a3f6a0d5155bdf01e47cceac79049bc4a010561
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-07-11 14:50:53 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity('<activity>', { type: 'WATCHING EVERYTHING...' });
 });
 
 client.on('message', message => {
