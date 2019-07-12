42734d539f83126fcc15240b1f693ded309f623e
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-07-11 21:33:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
         let status = statuses[Math.floor(Math.random()*statuses.length)];
          client.user.setPresence({activity: {name: status}, status: 'online'});
         
-    },1000);
-    client.user.setStatus('dnd').then(console.log).catch(console.error);
+    },1000)
+    //client.user.setStatus('dnd').then(console.log).catch(console.error);
     console.log('I am ready!');
 });
 
