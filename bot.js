f5a523d74d7c90b4829039827ac264b46eea6c6b
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-08-15 19:57:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ client.on('message', message => {
     if(message.content.startsWith('jah hash')){
         let text = message.content.substr(9);
         
-        let shasum = crypto.createHash('sha256').upadate(text).digest('hex');
+        let shasum = crypto.createHash('sha256').update(text).digest('hex');
         
         message.repy(shasum);
     }
