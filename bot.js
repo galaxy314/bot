40392ad655dc32bc9129d4379c5735ed865acb00
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-08-16 23:06:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -202,9 +202,11 @@ client.on('message', message => {
     
     if(command === 'jah speak'){
         if(message.member.voiceChannel){
-            message.member.voiceChannel.join().then(connection => {
-                message.reply('Jah has joined')
-            });   
+            message.member.voiceChannel.join()
+            .then(connection => { // Connection is an instance of VoiceConnection
+                message.reply('I have successfully connected to the channel!');
+            })
+            .catch(console.log);
         }else{
             message.reply('you need to be in a voice channel for me to join');   
         }
