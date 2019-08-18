067d4a7f60cb36feecd726cd2be1957a9ce1ca28
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-08-17 21:43:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,8 +193,8 @@ client.on('message', message => {
     }
     
     if(message.author.id === '186874605443612672' && command === 'jah heart'){
-        //const hearts = client.emojis.find(emoji => emoji.name === "hearts");
-        message.react('612460700954001428')
+        const hearts = client.emojis.find(emoji => emoji.name === 'hearts');
+        message.react(hearts)
         .then(console.log)
         .catch(console.error);   
     }
