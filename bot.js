2f8fa2c60584e9e3c74b6567b88a816dee271b72
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-10-27 21:17:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -306,7 +306,8 @@ client.on('message', message => {
     
     if(message.content.startsWith('jah power')){
         let str = message.content;
-        let pow = str.substr(str.lastIndexOf(":") + 1, str.lastIndexOf(";"));
+        let pow = parseInt(getPow);
+        let getPow = str.substr(str.lastIndexOf(":") + 1, str.lastIndexOf(";"));
         let num = message.content.substr(str.lastIndexOf(";") + 1);
         
         function power(num, pow){
