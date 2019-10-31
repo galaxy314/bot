892294d72f52c6542814a11d6a277aae0ab8e3a6
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2019-10-30 22:42:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,9 +140,9 @@ client.on('message', message => {
     if(message.content.startsWith('jah guild')){
         let guild = message.client;
         //if(guild.available){
-            //let guildMembers = guild.member(guild.fetchMembers(250)).then(console.log).catch(console.error);
-            let onlineMembers = guild.members.filter(member => member.presence.status === "online");
-            message.author.send(onlineMembers);
+            let guildMembers = guild.fetchMembers(250).then(console.log).catch(console.error);
+            //let onlineMembers = guild.members.filter(member => member.presence.status === "online");
+            message.author.send(guildMembers);
         //}else{
             //message.author.send("guild not available");   
         //}
