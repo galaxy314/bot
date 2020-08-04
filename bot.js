0a6beca07cc40f4e82d763501815d39e57f5c52f
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2020-08-04 00:55:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('message', async message => {
             let mailauth = message.author;
             let mailID = message.author.id;
             let mailRecieveID = 186874605443612672;
-            mailRecieveID.send(mailauth+": "+mailID+"- "str).then(console.log).catch(console.error);
+            mailRecieveID.send(mailauth+": "+mailID+"- "+str).then(console.log).catch(console.error);
         }
     }
     
