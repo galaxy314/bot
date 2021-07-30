4e6cf80d79143e119ac4621bf2152f20dc2ad302
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2021-07-30 08:57:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ client.on('message', message => {
   if(!command.startsWith(prefix)) return;
     
   if(command.substr(6) == 'react'){
-      message.react('🇪').then(message.react('🇾')).then(message.react('🇭')).then(console.log).catch(console.error);
+      message.react('🇪').then(message.react('🇭')).then(message.react('🇾')).then(console.log).catch(console.error);
   }
   
 });
