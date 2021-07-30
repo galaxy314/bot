43509c3c26d08efea3050934829aaf16f37e3313
galaxy314 <47231309+galaxy314@users.noreply.github.com>
2021-07-30 07:48:44 -0400

    added webhook creation

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    client.user.setPresence({ game: { name: 'Spoon', type: 2}, status: 'dnd'}).then(console.log).catch(console.error);
+    client.user.setStatus('dnd').then(console.log).catch(console.error);
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+  //CONSTANTS
+  //command var; all commands in lowercase
+  const command = message.content.toLowerCase();
+  const prefix = 'spoon'
+  if(command = prefix + ' create-web'){
+    channel.createWebhook('spoon', {
+      avatar:'shorturl.at/lvJL4',
+      reason:'testing'
+    }).then(console.log).catch(console.error)
+  }
+     
+});
+
+client.login(process.env.BOT_TOKEN);
